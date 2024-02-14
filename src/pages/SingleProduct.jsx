45541c1618bc0d603f8d6e7fff6d5c7179068afa
Deformator <kostneko30@gmail.com>
2024-02-14 03:35:49 -0500

    changes

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -10,7 +10,14 @@ import star from "../assets/images/review-star.svg";
 import twitterIcon from "../assets/images/twitter.svg";
 import linkedinIcon from "../assets/images/linkedin.svg";
 import facebookIcon from "../assets/images/facebook.svg";
+import descriptionSofa1 from "../assets/images/single-product__description-sofa1.png";
+import descriptionSofa2 from "../assets/images/single-product__description-sofa1.png.png";
+import ourProducts1 from "../assets/images/our-products1.png";
+import ourProducts2 from "../assets/images/our-products2.png";
+import ourProducts3 from "../assets/images/our-products3.png";
+import ourProducts4 from "../assets/images/our-products4.png";
 import { useState } from "react";
+import ProductCart from "../components/ProductCart";
 function SingleProduct() {
   const [countProduct, setCountProduct] = useState(0);
   const incrementCountProduct = () => {
@@ -20,7 +27,7 @@ function SingleProduct() {
     setCountProduct(countProduct - 1);
   };
   return (
-    <div className="single-product">
+    <main className="single-product">
       <div className="single-product__panel">
         <p>Home</p>
         <p>
@@ -77,7 +84,7 @@ function SingleProduct() {
             <div className="single-product-line reviews-line"></div>
             <p>5 Customer Review</p>
           </div>
-          <p className="single-product__description">
+          <p className="single-product__descriptionn">
             Setting the bar as one of the loudest speakers in its class, the
             Kilburn is a compact, stout-hearted hero with a well-balanced audio
             which boasts a clear midrange and extended highs for a sound.
@@ -134,14 +141,81 @@ function SingleProduct() {
                 <div className="single-product__socials">
                   <img src={facebookIcon} alt="image" />
                   <img src={linkedinIcon} alt="image" />
-                  <img src={twitterIcon} width="20px" height="20px" alt="image" />
+                  <img
+                    src={twitterIcon}
+                    width="20px"
+                    height="20px"
+                    alt="image"
+                  />
                 </div>
               </p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="single-product__description">
+        <div className="single-product__description__block-first">
+          <p>Description</p>
+          <p>Additional information</p>
+          <p>Reviews[5]</p>
+        </div>
+        <div className="single-product__description__block-second">
+          <p>
+            Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn
+            portable active stereo speaker takes the unmistakable look and sound
+            of Marshall, unplugs the chords, and takes the show on the road.
+          </p>
+          <p>
+            Weighing in under 7 pounds, the Kilburn is a lightweight piece of
+            vintage styled engineering. Setting the bar as one of the loudest
+            speakers in its class, the Kilburn is a compact, stout-hearted hero
+            with a well-balanced audio which boasts a clear midrange and
+            extended highs for a sound that is both articulate and pronounced.
+            The analogue knobs allow you to fine tune the controls to your
+            personal preferences while the guitar-influenced leather strap
+            enables easy and stylish travel.
+          </p>
+        </div>
+        <div className="single-product__description__block-third">
+          <div className="single-product__description__block">
+            <img src={descriptionSofa1} alt="image" />
+          </div>
+          <div className="single-product__description__block">
+            <img src={descriptionSofa2} alt="image" />
+          </div>
+        </div>
+      </div>
+      <div className="releated-products">
+        <p className="releated-products__title">Releated products</p>
+        <ProductCart
+          title="Syltherine"
+          description="Stylish cafe chair"
+          imgPath={ourProducts1}
+          price="Rp 2.500.000"
+          discountPrice="Rp 3.500.000"
+        />
+        <ProductCart
+          title="Leviosa"
+          description="Stylish cafe chair"
+          imgPath={ourProducts2}
+          price="Rp 2.500.000"
+        />
+        <ProductCart
+          title="Lolito"
+          description="Luxury big sofa"
+          imgPath={ourProducts3}
+          price="Rp 7.000.000"
+          discountPrice="Rp 14.000.000"
+        />
+        <ProductCart
+          title="Respira"
+          description="Outdoor bar table and stool"
+          imgPath={ourProducts4}
+          price="Rp 500.000"
+        />
+        <div className="releated-products__show-more">Show More</div>
+      </div>
+    </main>
   );
 }
 
